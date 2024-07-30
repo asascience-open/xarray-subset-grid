bf91ec8e710c9bd986f9541f1bdbe091d3fe8c41
Matthew Iannucci <mpiannucci@gmail.com>
2024-07-30 16:46:37 -0400

    Add reusable subset examples

--- xarray_subset_grid/grids/sgrid.py
+++ xarray_subset_grid/grids/sgrid.py
@@ -16,10 +16,14 @@ class SGridSelector(Selector):
     def __init__(
         self,
         polygon: list[tuple[float, float]] | np.ndarray,
+        grid_topology_key: str,
+        grid_topology: xr.DataArray,
         subset_masks: list[tuple[list[str], xr.DataArray]],
     ):
         super().__init__()
         self.polygon = polygon
+        self._grid_topology_key = grid_topology_key
+        self._grid_topology = grid_topology
         self._subset_masks = subset_masks
 
     def select(self, ds: xr.Dataset) -> xr.Dataset:
@@ -33,7 +37,6 @@ class SGridSelector(Selector):
 
             # Now we can use the mask to subset the data
             ds_subset = ds_subset[mask[0]].where(ds_subset.subset_mask, drop=True).drop_encoding()
-            ds_subset = ds_subset.drop_vars("subset_mask")
 
             # Add the subsetted dataset to the list for merging
             ds_out.append(ds_subset)
@@ -128,7 +131,12 @@ class SGrid(Grid):
 
             subset_masks.append((vars, subset_mask))
 
-        return SGridSelector(polygon=polygon, subset_masks=subset_masks)
+        return SGridSelector(
+            polygon=polygon,
+            grid_topology_key=grid_topology_key,
+            grid_topology=grid_topology,
+            subset_masks=subset_masks,
+        )
 
 
 def _get_sgrid_dim_coord_names(
