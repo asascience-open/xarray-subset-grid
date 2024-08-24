1641c3a1d2076894c3cfe7adc92078a9b184181b
omkar-334 <omkarkabde@gmail.com>
2024-08-24 23:33:18 +0530

    selector methods

--- xarray_subset_grid/selector.py
+++ xarray_subset_grid/selector.py
@@ -1,5 +1,9 @@
+import hashlib
+import os
+import pickle
 from abc import abstractmethod
 
+import numpy.testing as npt
 import xarray as xr
 
 
@@ -13,6 +17,24 @@ class Selector:
     select should return a new xarray dataset that is a subset of the
     input dataset and must be implemented by the subclass.
     """
+    def __init__(self, path=None):
+        """Initialize the Selector instance.
+        If a filename is provided, attempt to load the selector from that file.
+        """
+        if path:
+            instance = self.load(path)
+            self.__dict__.update(instance.__dict__)
+            self.__class__ = instance.__class__
+
+    __hash__ = None
+
+    def __eq__(self, other):
+        if not isinstance(other, Selector):
+            return NotImplemented
+        return npt.assert_equal(self.__dict__, other.__dict__) is None
+
+    def __repr__(self):
+        return f"{self.__class__} - {self.name}"
 
     @abstractmethod
     def select(self, ds: xr.Dataset) -> xr.Dataset:
@@ -28,3 +50,24 @@ class Selector:
         datasets..
         """
         raise NotImplementedError()
+
+    def get_cache_filename(self, polygon=None):
+        if not polygon:
+            polygon = self.polygon
+        hashname = hashlib.md5(str(polygon).encode()).hexdigest()
+        filename = f"{self.name}_{hashname[:8]}.pkl"
+        return filename
+
+    def save(self):
+        """Save the selector to the cache file."""
+        filename = self.get_cache_filename()
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+        return filename
+
+    def load(self, path):
+        if os.path.exists(path) and os.path.isfile(path):
+            with open(path, "rb") as f:
+                return pickle.load(f)
+        else:
+          raise FileNotFoundError(f"The file '{path}' does not exist.")
